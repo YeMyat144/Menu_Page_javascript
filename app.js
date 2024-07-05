eb24f70344e35add90d7e95708778ca68a658e9f
Ye Myat Moe <149359862+YeMyat144@users.noreply.github.com>
2024-07-05 22:14:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
+// const categoryBtns = categories
+//     .map(function (category) {
+//       return `<button type="button" class="filter-btn" data-id=${category}>
+//           ${category}
+//         </button>`;
+//     })
+//     .join("");
+
+//   btnContainer.innerHTML = categoryBtns;
+//   const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+//   console.log(filterBtns);
+
+//   filterBtns.forEach(function (btn) {
+//     btn.addEventListener("click", function (e) {
+//       // console.log(e.currentTarget.dataset);
+//       const category = e.currentTarget.dataset.id;
+//       const menuCategory = menu.filter(function (menuItem) {
+//         // console.log(menuItem.category);
+//         if (menuItem.category === category) {
+//           return menuItem;
+//         }
+//       });
+//       if (category === "all") {
+//         diplayMenuItems(menu);
+//       } else {
+//         diplayMenuItems(menuCategory);
+//       }
+// const categoryBtns = categories
+//     .map(function (category) {
+//       return `<button type="button" class="filter-btn" data-id=${category}>
+//           ${category}
+//         </button>`;
+//     })
+//     .join("");
+
+//   btnContainer.innerHTML = categoryBtns;
+//   const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+//   console.log(filterBtns);
+
+//   filterBtns.forEach(function (btn) {
+//     btn.addEventListener("click", function (e) {
+//       // console.log(e.currentTarget.dataset);
+//       const category = e.currentTarget.dataset.id;
+//       const menuCategory = menu.filter(function (menuItem) {
+//         // console.log(menuItem.category);
+//         if (menuItem.category === category) {
+//           return menuItem;
+//         }
+//       });
+//       if (category === "all") {
+//         diplayMenuItems(menu);
+//       } else {
+//         diplayMenuItems(menuCategory);
+//       }
 const menu = [
   {
     id: 1,
